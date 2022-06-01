ef83fc2b69645bf3bd0b7041e3aea48f568b37cb
Natalia <krotova.nat5656@gmail.com>
2022-06-01 15:39:06 +0300

    chsnges

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -1,9 +1,7 @@
 // "use strict";
 
 const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
-
 const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
-
 const marks = [4, 5, 5, 3, 4, 5];
 
 // 1. Розділіть студентів на пари(хлопець + дівчина) для работи над проєктом. У вас повинен вийти вкладений масив з парами студентів: [["Олександр", "Олена"], [..], [...]];
@@ -11,57 +9,60 @@ const marks = [4, 5, 5, 3, 4, 5];
 console.log(`1.Розділіть студентів на пари(хлопець + дівчина) для работи над проєктом.`)
 
 const girls = students.filter((elem) => {
-    return elem.lastIndexOf('а') === elem.length - 1;
-    
+    return elem.lastIndexOf('а') === elem.length - 1;    
 });
-console.log (girls);
-
+// console.log (girls);
 const boys = students.filter((elem) => {
     return elem.lastIndexOf('а') !== elem.length - 1;
-
 });
-console.log (boys);
-
-let allStudents = (students) => {
-
-return boys.map((boys, i) => [boys + ` ` + `i` + ` ` + girls[i]]);
+// console.log (boys);
+const allStudents = (students) => {
+return boys.map((boys, i) => [boys, girls[i]]);
 }
 console.log (allStudents(students));
-
-let allStudent = allStudents();
+const allStudent = allStudents();
 
 // 2.Зіставте пари з попереднього завдання та теми проєктів, над якими студенти будуть працювати. Повинен вийти вкладений масив виду: [["Олександр і Олена", "Теорія автоматів"], [...], [...]]
 
 console.log(`2.Зіставте пари з попереднього завдання та теми проєктів, над якими студенти будуть працювати.`)
-
-let taskforСouples = [];
-
-for (let i = 0; i < allStudent.length && i < themes.length; i++){
-
-    taskforСouples.push([allStudent[i] + ` ` + `,` + ` ` + themes[i]] );
+ 
+function taskforСouples () {
+    const taskforСouple= [];
+    for(let i = 0; i < allStudent.length; i++){
+    taskforСouple.push([allStudent[i].join(' і ') , themes[i]])
     };
-console.log(taskforСouples);
+    return taskforСouple;
+    };
+    console.log (taskforСouples())
+
+  const  taskforСouples1 = taskforСouples()
+
 
 // 3.Зіставте оцінки(marks) зі студентом(students): [["Саша", 4], [...], [...]]
 
 console.log(`3.Зіставте оцінки(marks) зі студентом(students): [["Саша", 4], [...], [...]]`)
 
-let studentmarks = [];
 
-for (let i = 0; i < students.length && i < marks.length; i++){
+function getStudenMarks(){
+let studentMarks = [];
 
-    studentmarks.push([students[i] + `,` + ` ` + marks[i]] );
-    };
-console.log(studentmarks);
+for (let i = 0; i < students.length; i++){
+
+    studentMarks.push([students[i] + `,` + ` ` + marks[i]] );
+}
+return studentMarks
+}
+
+console.log(getStudenMarks(students, marks));
+
+
 
 // 4.Поставте кожній парі випадкову оцінку(від 1 до 5) за проєкт(тут функція буде нечистою, але не повинна мутувати массив): [["Олександр і Олена", "Теорія автоматів", 5], [...], [...]]
 
 console.log(`4.Поставте кожній парі випадкову оцінку(від 1 до 5) за проєкт`)
 
-let randomEvaluation = [];
+const randomEvaluation = taskforСouples1.map(function(el) {
+   return  [el + ` , ` +  Math.floor((Math.random()* 5)+1)];
+});
 
-for (let i = 0; i < taskforСouples.length; i++){
-
-    randomEvaluation.push([taskforСouples[i] + `,` + ` ` +  Math.floor((Math.random()* 5)+1)]);
-    };
 console.log(randomEvaluation);
