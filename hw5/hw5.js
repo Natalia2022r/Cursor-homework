c1d99762f3da05323f0f02d617c8ab428b4dcaaf
Natalia <krotova.nat5656@gmail.com>
2022-06-08 13:18:39 +0300

    fixed

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -3,6 +3,9 @@
 
 const getRandomArray = (length, min, max) => {
 const randomArray = [];
+if (min > max){
+    return `Введіть правільні значення`;
+}
 for (let i = 0; i < length; i++){
 randomArray.push(Math.floor(Math.random() * (max - min) + min));
 } 
@@ -15,7 +18,7 @@ return randomArray;
 
 
 const getModa = (...numbers) =>{
-    let arrModa = (numbers.map(elem => parseInt(elem)));
+    const arrModa = numbers.map((elem) => Number(elem)).filter((item) => parseInt(item) === item);
     const uniqSet = new Set();
     for (let i = 0; i < arrModa.length; i++) {
         for (let j = 0; j < i; j++)
@@ -30,7 +33,7 @@ const getModa = (...numbers) =>{
 // Приклад: getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) –> 34.4
 
 const getAverage = (...numbers) =>{
-let arr = (numbers.map(elem => parseInt(elem)));
+let arr = numbers.map(elem => Number(elem)).filter((item) => parseInt(item) === item);
 let sum = 0;
 for(let i = 0; i < arr.length; i++){
 sum += arr[i];
@@ -45,7 +48,7 @@ return sum / arr.length;
 
 const getMedian1 = (...numbers) =>{
 
-    let arr = (numbers.map(elem => parseInt(elem)));
+    let arr = numbers.map(elem => Number(elem)).filter((item) => parseInt(item) === item);
     const median = arr.sort(function(a, b){
     return a-b;
     });
@@ -71,22 +74,29 @@ const countPositiveNumbers =(...numbers) => numbers.filter(elem => elem > 0).len
 
 // 7.Створіть функцію getDividedByFive(...numbers) – яка відфільтрує усі елементи в масиві та залишить тільки ті, які діляться на ціло на 5 Приклад: getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) -> [55, 55]
 
-const  getDividedByFive  =(...numbers) => numbers.filter(elem => elem % 5 === 0);
+const  getDividedByFive  =(...numbers) => {
+    let arr = numbers.map(elem => Number(elem)).filter((item) => parseInt(item) === item);
+    let i = 0;
+    if (arr[i] === 0){
+        return `Введіть значення`
+    }
+     return arr.filter(elem => elem % 5 === 0);}
+
 
 /*8.Створіть функцію replaceAll(string) – яка 1) розіб'є фразу на слова, 2) замінить погані слова на зірочки (*). При вирішенні цього завдання необхідно розбити масив на слова за допомогою функції .split(" "), після чого масив необхідно буде склеїти .join(" ") Погані слова: shit та fuck. Передбачте можливість розширювати список цих слів у майбутньому.
 Приклад: replaceAll("Are you fucking kidding?") -> "Are you ****ing kidding?" Приклад: replaceAll("Holy shit!") -> "Holy ****!" Приклад: replaceAll("It's bullshit!") -> "It's bull****!"*/
 
 function replaceAll(string) {
 
-    let reg = /(shit)|(Fuck)/gi;
-
-    let newstr = string.replace(reg, '****');
+    let reg = /(shit)|(Fuck)/gi;  
+    let newstr = string.replace(reg,  function(a, b, c) {
+    return c.replace(/./g, '*');});
  
     return newstr;
 
 };
 
-console.log(replaceAll('Are you fucking kidding?'));
+// console.log(replaceAll('Are you fucking kidding?'));
 
 // 9.Створіть функцію divideByThree(word), яка розбиває кожне слово на умовні склади по 3 букви. Якщо букв менше трьох – не розбиває. Пробіли завжди видаляються. Рядок приводится до нижнього регістру. 
 // Приклад: divideByThree("Commander") -> ["com", "man", "der"] 
@@ -129,37 +139,37 @@ const divideByThree = (word) => {
  }
 return res;
  }
- 
-// console.log(generateCombinations("man"));
+//  console.log(generateCombinations("man"));
 
 
-console.log('getRandomArray(10, 3, 100)=> ', getRandomArray(10, 3, 100));
 
-console.log('getModa(4, 75, 62, 5, 2)=> ', getModa(4, 75, 62, 5, 2));
-console.log('getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)=> ',
-    getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+// console.log('getRandomArray(10, 3, 100)=> ', getRandomArray(10, 3, 100));
 
-console.log('getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)=> ',
-    getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+// console.log('getModa(4, 75, 62, 5, 2)=> ', getModa(4, 75, 62, 5, 2));
+// console.log('getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)=> ',
+//     getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 
-console.log('getMedian1(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)=>', 
-getMedian1(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
-console.log('getMedian1(1, 2, 3, 4)=>', getMedian1(1, 2, 3, 4));
+// console.log('getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)=> ',
+//     getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+
+// console.log('getMedian1(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)=>', 
+// getMedian1(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+// console.log('getMedian1(1, 2, 3, 4)=>', getMedian1(1, 2, 3, 4));
 
 
-console.log('filterEvenNumbers(1, 2, 3, 4, 5, 6)=> ', filterEvenNumbers(1, 2, 3, 4, 5, 6));
+// console.log('filterEvenNumbers(1, 2, 3, 4, 5, 6)=> ', filterEvenNumbers(1, 2, 3, 4, 5, 6));
 
-console.log('countPositiveNumbers(1, -2, 3, -4, -5, 6)=>', countPositiveNumbers(1, -2, 3, -4, -5, 6));
+// console.log('countPositiveNumbers(1, -2, 3, -4, -5, 6)=>', countPositiveNumbers(1, -2, 3, -4, -5, 6));
 
-console.log('getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)=>',
-    getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+// console.log('getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)=>',
+//     getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 
-console.log(`replaceAll('Are you fucking kidding?', 'shit', 'Fuck')=>`,
-    replaceAll('Are you fucking kidding?', 'shit', 'Fuck'));
-console.log('replaceAll(`It\'s bullshit!`=>', replaceAll(`It's bullshit!`));
+// console.log(`replaceAll('Are you fucking kidding?', 'shit', 'Fuck')=>`,
+//     replaceAll('Are you fucking kidding?', 'shit', 'Fuck'));
+// console.log('replaceAll(`It\'s bullshit!`=>', replaceAll(`It's bullshit!`));
 
-console.log(`divideByThree('Commander')=> `, divideByThree('Commander'));
-console.log(`divideByThree('live')=> `, divideByThree('live'));
+// console.log(`divideByThree('Commander')=> `, divideByThree('Commander'));
+// console.log(`divideByThree('live')=> `, divideByThree('live'));
 
-console.log(`generateCombinations('man')=>`, generateCombinations('man'));
-console.log(`generateCombinations('ol')=>`, generateCombinations('ol'));
+// console.log(`generateCombinations('man')=>`, generateCombinations('man'));
+// console.log(`generateCombinations('ol')=>`, generateCombinations('ol'));
