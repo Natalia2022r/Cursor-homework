f1ad452e02fca70ac3c9532731637817202b0a64
Natalia <krotova.nat5656@gmail.com>
2022-06-25 18:49:08 +0300

    Finaly

--- hw11/hw11.js
+++ hw11/hw11.js
@@ -1,14 +1,16 @@
 `use strict`
 
 function getRandomChinese(length){
+    if(length <= 0 || !Number.isInteger(length)){return console.log("Введіть число!!!")}
    let result = [];
    const chineseString = new Promise((res) => {
-   setTimeout(function (){
-    for (let i = 0; i < length; i++){ 
-    let sign = String(Date.now()).slice(-5);
-     result.push(String.fromCharCode(sign))
-     res(result.join(''))}
-    }, length*50)
+     setTimeout(function (){
+       for (let i = 0; i < length; i++){ 
+        let sign = String(Date.now()).slice(-5);
+    result.push(String.fromCharCode(sign))
+    }
+    res(result.join(''));
+    }, 50);
 })
 chineseString.then((res) => {console.log(res)});
 return chineseString;
