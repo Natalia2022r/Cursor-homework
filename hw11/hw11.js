01dbeba6ee0a416f0147ce6f2dc0be10e3ab20b4
Natalia2022r <99670282+Natalia2022r@users.noreply.github.com>
2022-07-13 13:06:55 +0300

    fix

--- hw11/hw11.js
+++ hw11/hw11.js
@@ -1,19 +1,26 @@
 `use strict`
 
-function getRandomChinese(length){
-    if(length <= 0 || !Number.isInteger(length)){return console.log("Введіть число!!!")}
-   let result = [];
-   const chineseString = new Promise((res) => {
-     setTimeout(function (){
-       for (let i = 0; i < length; i++){ 
-        let sign = String(Date.now()).slice(-5);
-    result.push(String.fromCharCode(sign))
-    }
-    res(result.join(''));
-    }, 50);
-})
-chineseString.then((res) => {console.log(res)});
-return chineseString;
+async function getRandomChinese(length) {
+  if(Number.isInteger(length) && length > 0){
+      let str = "";
+
+      for (let i = 0; i < length; i++) {
+          const symbolPromise = new Promise (function(resolve) {
+              setTimeout (function() {
+                  const sign = String(Date.now()).slice(-5);
+                  const chineseSymbol = String.fromCharCode(sign);
+                  resolve(chineseSymbol)
+              }, 50)
+          })
+
+          str = str.concat(await symbolPromise);
+         
+      }
+      return str;
+  } else {
+      return "Wrong!"
+  }
 }
-getRandomChinese(5);
+
+getRandomChinese(5).then((resolve) => console.log(resolve));
 
